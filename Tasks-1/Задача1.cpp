78b0d37f7475d72fc9310d6a878aa0efd38ba42e
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-06-26 18:14:17 +0300

    Create Задача1.cpp

---
+++ Tasks-1/Задача1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string* names = new string[100];
+    int nameCount = 0;
+    string name;
+    char startLetter;
+
+    cout << "Въведете имена (въведете '0' за край на въвеждането):" << endl;
+    while (getline(cin, name) && name != "0" && nameCount < 100) {
+        names[nameCount++] = name;
+    }
+
+    cout << "Въведете началната буква, с която искате да започват имената: ";
+    cin >> startLetter;
+
+    cout << "Имената, които започват с '" << startLetter << "' са:" << endl;
+    for (int i = 0; i < nameCount; ++i) {
+        if (!names[i].empty() && names[i][0] == startLetter) {
+            cout << names[i] << endl;
+        }
+    }
+
+    delete[] names;
+
+    return 0;
+}
