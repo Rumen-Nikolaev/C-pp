40e2d7a50151f45303ebd4726f71a555a414c01a
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-08-15 01:58:54 +0300

    Create Task5-1.cpp

---
+++ Tasks5/Task5-1/Task5-1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, k, ans, i;
+    int arr[] = { 1, 0, 4, 4, 5, 9 };
+    n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Nashiq masiv: " << endl;
+    
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+    
+    for(k = 1; k <= 4; k++) 
+    {
+        for(int i = 0; i < n; i++) {
+            if (arr[i] == k) 
+            {
+                cout << "Ti x li si ? ";
+                ans = i;
+                cout << "Elementut " << k << " e x i na mqstoto na " << ans << " index" << endl;
+            }
+        }
+    }
+    
+    cout << "Broi zavurtaniq na ti x li si: " << i;
+    
+
+    return 0;
+}
