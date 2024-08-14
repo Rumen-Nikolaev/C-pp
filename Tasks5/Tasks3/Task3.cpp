2243cbce8eb241b5c85990ae1e5485b2d759a195
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-08-14 18:03:08 +0300

    Create Task3.cpp

---
+++ Tasks5/Tasks3/Task3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    int Sum = 0;
+    
+    cin >> n;
+    
+    for(int i = 1; i <= n; i++) {
+        Sum += i;
+    }
+    
+    cout << Sum << endl;
+
+    return 0;
+}
