50fac27650f846c143fad371e48728ab061ef15a
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-08-15 03:34:26 +0300

    Create Task7-4.cpp

---
+++ Tasks5/Task7-4/Task7-4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {7, 3, 2, 1, 6};
+    int count, n = 5;
+    
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                count++;
+            }
+        }
+    }
+    
+    cout << "Sortiran masiv: \n";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+    cout << "Broi inversii: " << cout << endl;
+    
+    return 0;
+}
