583cf2e92ad49242f3bf200954183c406ecf500d
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-08-15 11:15:05 +0300

    Create Task9-3.cpp

---
+++ Tasks5/Task9-3/Task9-3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<vector<int>> matrica;
+    int num, x;
+    cin >> num;
+    
+    for(int i = 0; i < num; i++) {
+        vector<int> arr;
+        for(int j = 0; j < num; j++) {
+            cin >> x;
+            arr.push_back(x);
+        }
+        matrica.push_back(arr);
+    }
+    
+    for(int r = 1, c = 0; r < num; r++, c++)
+    {
+        cout << matrica[r][c] << endl;
+    }
+
+    
+    return 0;
+}
