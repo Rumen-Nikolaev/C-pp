025dd0bd4cd85558346057a5440796c190bfb37c
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-08-15 03:15:01 +0300

    Create Task6-3.cpp

---
+++ Tasks5/Task6-3/Task6-3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[8]{3, 5, 7, 2, 1, 6, 7, 8};
+    int b[8];
+    int index = 0, min = a[0], max = a[0], size = sizeof(a) / sizeof(a[0]);
+    
+    for(int i = 0; i < size; i++) {
+        if (max < a[i]) {
+            max = a[i];
+        }
+    }
+    
+    b[size-1] = max;
+    
+    for(int i = 0; i < size - 1; i++) {
+        index = 0;
+        min = a[0];
+        for(int j = 0; j < size - 1; j++) {
+            if (a[j] < min) {
+                min = a[j];
+                index = j;
+            }
+        }
+        a[index] = max;
+        b[i] = min;
+    }
+    
+    for(int i = 0; i < size; i++) {
+        cout << b[i] << endl;
+    }
+    
+
+    return 0;
+}
