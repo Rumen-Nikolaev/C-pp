c39560cbbcb1ec83bf74b1833781e2c0764c899e
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-09-19 20:48:24 +0300

    Create Task5-2-Important.cpp

---
+++ Tasks5/Task5-2-Important.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Функция за пресмятане на sin(x) с факториел
+double sinWithFactorial(double x) {
+    double sum = x;  // Първи член
+    double term = x;
+    int n = 1;
+
+    while (fabs(term) > 0.000001) {
+        term *= -x * x / ((2 * n) * (2 * n + 1));  // Следващ член
+        sum += term;
+        n++;
+    }
+
+    return sum;
+}
+
+int main() {
+    double degrees, radians;
+
+    // Въвеждаме ъгъл в градуси
+    cout << "Въведете ъгъл в градуси: ";
+    cin >> degrees;
+
+    // Преобразуване на градусите в радиани
+    radians = degrees * M_PI / 180.0;
+
+    // Изчисляваме sin(x) с факториел
+    cout << "sin(" << degrees << " градуса) = " << sinWithFactorial(radians) << endl;
+
+    return 0;
+}
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Функция за пресмятане на sin(x) с факториел
+double sinWithFactorial(double x) {
+    double sum = x;  // Първи член
+    double term = x;
+    int n = 1;       // Степен на факториела
+    double factorial = 1;  // Факториел
+
+    while (fabs(term) > 0.000001) {
+        factorial *= (2 * n) * (2 * n + 1);  // Пресмятане на факториела
+        term *= -x * x / factorial;  // Следващ член от реда
+        if (isinf(term)) {
+            cout << "Препълване при член " << n << endl;
+            break;
+        }
+        sum += term;  // Добавяме го към сумата
+        n++;
+    }
+
+    return sum;
+}
+
+int main() {
+    double degrees, radians;
+
+    // Въвеждаме ъгъл в градуси
+    cout << "Въведете ъгъл в градуси: ";
+    cin >> degrees;
+
+    // Преобразуване на градусите в радиани
+    radians = degrees * M_PI / 180.0;
+
+    // Изчисляваме sin(x) с факториел
+    cout << "sin(" << degrees << " градуса) = " << sinWithFactorial(radians) << endl;
+
+    return 0;
+}
+
