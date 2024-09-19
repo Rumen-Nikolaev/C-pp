b87a5b0e3bd36775015ad495ee76d500832d9a17
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-09-19 22:07:11 +0300

    Update Task6-3.cpp

--- Tasks5/Task6-3/Task6-3.cpp
+++ Tasks5/Task6-3/Task6-3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int a[]{2, 1, 4, 0, 6, 4, 1, 4, 7};
-    int b[];
+    int b[9];
     int index = 0, min = a[0], max = a[0], size = sizeof(a) / sizeof(a[0]);
     
     for(int i = 0; i < size; i++) {
