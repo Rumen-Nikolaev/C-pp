6f23348cf7b59cad1d21fa09aa72d743d5c7be46
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-09-21 10:43:39 +0300

    Update Task2-1.cpp

--- Tasks5/Task2-1/Task2-1.cpp
+++ Tasks5/Task2-1/Task2-1.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
 using namespace std;
 
+int gcd(int a, int b) {
+  if (a == 0)
+      return b;
+  if (b == 0)
+      return a; 
+  if (a == b)
+      return a;
+  if (a > b)
+  {
+      return gcd(a-b), b);
+  }
+  else {
+       return gcd(a, b-a);
+  }
+}
+
+int main() {
+  int x, y;
+
+  cout << "Enter two integers: << endl;
+  cin >> x >> y;
+
+  cout << gcd(x, y);
+
+  return 0;
+}
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
 int gcd(int x, int y) {
   if (y == 0)
   return x;
