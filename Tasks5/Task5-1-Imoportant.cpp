c0ddc5c416e35f514f7dd6e906eddebecc669bf1
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-09-22 10:48:06 +0300

    Update Task5-1-Imoportant.cpp

--- Tasks5/Task5-1-Imoportant.cpp
+++ Tasks5/Task5-1-Imoportant.cpp
@@ -105,3 +105,64 @@ int main() {
 }
 
 
+#include <iostream>
+#include <iomanip> // За std::setprecision
+#include <cmath>   // За fabs
+
+using namespace std;
+
+int main() {
+    double pi = 0.0;
+    double numerator = 1.0;
+    int sign = 1;
+    double previous_value = 0.0;
+    double difference = 1.0;
+    int i = 0;
+
+    while (difference >= 0.0001) {
+        previous_value = pi;
+        pi += (numerator / (2 * i + 1)) * sign;
+        sign *= -1;
+        ++i;
+        difference = fabs(pi - previous_value);
+    }
+
+    pi *= 4; // Завършване на формулата за π
+    cout << fixed << setprecision(15) << "π ≈ " << pi << endl;
+
+    return 0;
+}
+
+#include <iostream>
+#include <iomanip> // За std::setprecision
+#include <cmath>   // За fabs
+
+using namespace std;
+
+int main() {
+    double pi = 0.0;
+    double numerator = 1.0;
+    int sign = 1;
+    double previous_value = 0.0;
+    double difference = 1.0;
+    int i = 0;
+
+    loop_start:
+    if (difference < 0.0001) goto end_loop;
+    
+    previous_value = pi;
+    pi += (numerator / (2 * i + 1)) * sign;
+    sign *= -1;
+    ++i;
+    difference = fabs(pi - previous_value);
+    goto loop_start;
+
+    end_loop:
+    pi *= 4; // Завършване на формулата за π
+    cout << fixed << setprecision(15) << "π ≈ " << pi << endl;
+
+    return 0;
+}
+
+
+
