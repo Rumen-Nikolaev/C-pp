c7918467d721f8a181e1af9eb1960bb86809b2b9
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-09-25 21:21:36 +0300

    Update Task5-3.cpp

--- Tasks5/Task5-3/Task5-3.cpp
+++ Tasks5/Task5-3/Task5-3.cpp
@@ -38,39 +38,41 @@ using namespace std;
 
 int main()
 {
-    int i;
     char arr[15] = {'F', '1', '0', '4', '4', '5', '9', 'N', 'i', 'k', 'o', 'l', 'a', 'e', 'v'};
     bool sequence_arr[7] = {false, false, false, false, false, false, false};
-    char sequence_check[7] = {'9', '8', '7', 'q', 'iu', 'u', 'o'}; 
+    char sequence_check[7] = {'9', '8', '7', 'q', 'i', 'u', 'o'};
     int sequence_index = 0;
-    int count = sizeof(arr) / sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]);
 
     cout << "Nashiq masiv: " << endl;
 
-    for(int i = 0; i < count; i++) {
+    for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
     cout << "\n";
 
-    for(int i = 0; i < 15; i++) {
-        if (arr[i] == sequence_check[sequence_index]) 
+    for (int i = 0; i < n; i++) {
+        if (sequence_index >= 7) {
+            break;
+        }
+
+        if (arr[i] == sequence_check[sequence_index])
         {
             cout << "Posledovatelnost(" << i + 1 << ") - ";
-            cout << sequence_arr[sequence_index] << "-> ";
+            cout << sequence_arr[sequence_index] << " -> ";
             sequence_arr[sequence_index] = true;
             cout << sequence_arr[sequence_index] << endl;
             sequence_index++;
         }
     }
-    
+
     cout << endl;
-    for(int i = 0; i < 7; i++) {
+    for (int i = 0; i < 7; i++) {
         cout << "Posledovatelnost(" << i + 1 << ") - ";
-        
+
         if (sequence_arr[i]) {
             cout << "Da" << endl;
-        }
-        else {
+        } else {
             cout << "Ne" << endl;
         }
     }
